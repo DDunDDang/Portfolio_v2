9ae18ccc370beff9099c31c77d566b237b674981
DDunDDang <thdwns0331@gmail.com>
2024-08-07 19:03:30 +0900

    프로젝트 헤더

---
+++ src/components/Project.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Project = () => {
+    return (
+        <div className="container flex items-center justify-center min-h-screen mx-auto">
+            <section className='max-w-4xl px-4' id='project'>
+                <header className='mb-6 text-2xl font-bold text-center'>
+                    <h2>Projects</h2>
+                </header>
+            </section>
+        </div>
+    )
+}
+
+export default Project;
