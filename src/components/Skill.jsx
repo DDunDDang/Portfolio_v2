e7b7c7e6e64d822dd06d5d8d58521c5237129a05
DDunDDang <thdwns0331@gmail.com>
2024-08-07 19:24:01 +0900

    파일명 소문자 일괄 변경(Test)

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -11,7 +11,7 @@ const Skill = () => {
     ]
 
     function changeSpace(name) {
-        return name.replace(" ", "_");
+        return name.toLowerCase().replace(" ", "_");
     }
     
     return (
