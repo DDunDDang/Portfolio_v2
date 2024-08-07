cbdcdee4e54aa095b5647c3140451026232ff73a
DDunDDang <thdwns0331@gmail.com>
2024-08-07 19:03:56 +0900

    메인, 스킬, 추가 및 가로 스크롤 기능

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
+import Main from './components/Main';
+import Skill from './components/Skill';
+import Project from './components/Project';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const containerRef = useRef(null);
+    const sectionRefs = {
+        main: useRef(null),
+        skill: useRef(null),
+        project: useRef(null),
+    };
+
+    useEffect(() => {
+        const container = containerRef.current;
+        let scrollPosition = 0;
+        let scrollAccumulator = 0;
+
+        const handleWheel = (e) => {
+            e.preventDefault();
+            scrollAccumulator += e.deltaY;
+
+            if (Math.abs(scrollAccumulator) >= 70) {
+                if (scrollAccumulator > 0 && scrollPosition < 2) {
+                    scrollPosition++;
+                } else if (scrollAccumulator < 0 && scrollPosition > 0) {
+                    scrollPosition--;
+            }
+        
+            container.style.transform = `translateX(-${scrollPosition * 100}vw)`;
+            scrollAccumulator = 0;
+        }
+    };
+
+    window.addEventListener('wheel', handleWheel, { passive: false });
+
+    return () => {
+        window.removeEventListener('wheel', handleWheel);
+        };
+    }, []);
+
+    return (
+        <div ref={containerRef} className="scroll-container flex w-[300vw] transition-transform duration-500 ease-in-out">
+            <section ref={sectionRefs.main} className="flex iems-center justify-center w-screen min-h-screen">
+                <Main />
+            </section>
+            
+            <section ref={sectionRefs.skill} className="flex flex-col items-center justify-center w-screen min-h-screen">
+                <Skill />
+            </section>
+
+            <section ref={sectionRefs.project} className="flex items-center justify-center w-screen min-h-screen">
+                <Project />
+            </section>
+        </div>
+    );
 }
 
 export default App;
