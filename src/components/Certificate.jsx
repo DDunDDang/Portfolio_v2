e8e529c4d84cd6cbb78c6148034c4dd09c8eefd6
DDunDDang <thdwns0331@gmail.com>
2024-08-18 17:12:41 +0900

    오타 수정 및 테두리 추가

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -3,8 +3,8 @@ import ComponetHeader from "./ComponentHeader";
 
 const Certificate = () => {
     const certificates = [
-        { id: 0, name: "SQL 개발자(SQLD)", date: "2023. 07. 07" },
-        { id: 1, name: "리눅스마스터1차", date: "2024.08.07" },
+        { id: 0, name: "SQL 개발자(SQLD)", date: "2023.07.07" },
+        { id: 1, name: "리눅스마스터 2급 1차", date: "2024.08.07" },
         { id: 2, name: "정보처리기사(예정)", date: "2024.09.10" },
     ];
 
@@ -13,12 +13,12 @@ const Certificate = () => {
             <ComponetHeader name="Certificates" />
             <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
                 {certificates.map((certificate) => (
-                    <li key={certificate.id} className="p-4 bg-gray-100 rounded-lg">
+                    <li key={certificate.id} className="p-4 bg-gray-100 rounded-lg shadow-lg">
                         <h3 className='mb-2 text-xl font-bold'>
                             {certificate.name}
                         </h3>
                         <p className="text-gray-600">
-                            Date: {certificate.date}
+                            {certificate.date}
                         </p>
                     </li>
                 ))}
